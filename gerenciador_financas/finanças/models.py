c34a04e9fed1b9d16b0e7ff2552ffca36cd7ba89
AthosSperber <athossperber123@gmail.com>
2024-08-27 17:10:24 -0300

    Initial commit

---
+++ gerenciador_financas/finanças/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Transacao(models.Model):
+    TIPO_CHOICES = [
+        ('R', 'Receita'),
+        ('D', 'Despesa'),
+    ]
+    tipo = models.CharField(max_length=1, choices=TIPO_CHOICES)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
+    categoria = models.CharField(max_length=50)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return f'{self.tipo} - {self.valor}'
+
+# Create your models here.
