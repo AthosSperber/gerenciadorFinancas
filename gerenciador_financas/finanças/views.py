c1954aa2e7f9a8d2dbab0390d86f39ce035f02fa
AthosSperber <athossperber123@gmail.com>
2024-08-29 00:27:49 -0300

    gráfico com ploty

--- gerenciador_financas/finanças/views.py
+++ gerenciador_financas/finanças/views.py
@@ -1,3 +1,5 @@
+import plotly.graph_objs as go
+from plotly.offline import plot
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Transacao
 from .forms import TransacaoForm
@@ -5,9 +7,17 @@ from .forms import TransacaoForm
 def transacoes_list(request):
     transacoes = Transacao.objects.all()
     saldo = sum([t.valor if t.tipo == 'R' else -t.valor for t in transacoes])
+
+    receitas = sum(t.valor for t in transacoes if t.tipo == 'R')
+    despesas = sum(t.valor for t in transacoes if t.tipo == 'D')
+
+    fig = go.Figure(data=[go.Bar(x=['Receitas', 'Despesas'], y=[receitas, despesas], marker_color=['green', 'red'])])
+    graphic = plot(fig, output_type='div')
+
     return render(request, 'finanças/transacoes_list.html', {
         'transacoes': transacoes,
         'saldo': saldo,
+        'graphics': graphic,
         })
 
 def transacao_create(request):
